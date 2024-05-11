f45afc82742c53cfe3da5c0070b380126f7d40b5
Zachary Westerman <westerman.zachary@gmail.com>
2024-05-11 12:02:40 -0500

    update installer

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 rm -f "$HOME/.local/bin/paisley"
 ln -s "$DIR/paisley" "$HOME/.local/bin/paisley"
 
-# for i in luasocket luafilesystem
-# do
-# 	sudo luarocks install $i
-# done
+for i in luasocket luafilesystem
+do
+	sudo luarocks install $i
+done
