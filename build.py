5d5478c40b9888fe0c80b5a0c7be60631c5dbfb0
Zachary Westerman <westerman.zachary@gmail.com>
2024-12-20 13:02:51 -0600

    Allow generating standalone lua program for systems without Paisley installed

--- build.py
+++ build.py
@@ -2,10 +2,13 @@
 
 import re
 from pathlib import Path
+from sys import argv
 
 require = re.compile(r'require +[\'"]([^\'"]+)[\'"]')
-debug   = re.compile(r'--\[\[minify-delete\]\].*?--\[\[/minify-delete\]\]', re.DOTALL)
-debug2  = re.compile(r'(--\[\[build-replace=([^\]]+)\]\].*?--\[\[/build-replace\]\])', re.DOTALL)
+debug = re.compile(
+    r'--\[\[minify-delete\]\].*?--\[\[/minify-delete\]\]', re.DOTALL)
+debug2 = re.compile(
+    r'(--\[\[build-replace=([^\]]+)\]\].*?--\[\[/build-replace\]\])', re.DOTALL)
 # comment = re.compile(r'(?<![\'"-])--(\[\[([^\]]|\](?!\]))*\]\]|[^\n]*)')
 # endline = re.compile(r'\n\n+')
 # spaces  = re.compile(r'[ \t]+\n')
@@ -19,95 +22,114 @@ string1 = re.compile(r'\'(\\.|[^\'])*\'')
 string2 = re.compile(r'"(\\.|[^"])*"')
 whitespace = re.compile(r'(?s)\s+')
 
+
 class Action:
-	PAD = 0
-	NOPAD = 1
-	IGNORE = 2
+    PAD = 0
+    NOPAD = 1
+    IGNORE = 2
+
 
 PATTERNS = (
-	(comment2, Action.IGNORE),
-	(comment1, Action.IGNORE),
-	(string1, Action.NOPAD),
-	(string2, Action.NOPAD),
-	(keyword, Action.PAD),
-	(operator, Action.NOPAD),
-	(whitespace, Action.IGNORE),
+    (comment2, Action.IGNORE),
+    (comment1, Action.IGNORE),
+    (string1, Action.NOPAD),
+    (string2, Action.NOPAD),
+    (keyword, Action.PAD),
+    (operator, Action.NOPAD),
+    (whitespace, Action.IGNORE),
 )
 
 Path('build/').mkdir(exist_ok=True)
 
 VERSION = open('version.txt', 'r').readline().strip()
 
+
 def minify(lua_source: str) -> str:
-	result = ''
-	while len(lua_source):
-		matched = False
-		for (pattern, action) in PATTERNS:
-			if m := pattern.match(lua_source):
-				matched = True
+    result = ''
+    while len(lua_source):
+        matched = False
+        for (pattern, action) in PATTERNS:
+            if m := pattern.match(lua_source):
+                matched = True
 
-				text = m.group(0)
-				lua_source = lua_source[len(text):]
+                text = m.group(0)
+                lua_source = lua_source[len(text):]
 
-				if action == Action.PAD:
-					result += text + ' '
-				elif action == Action.NOPAD:
-					if len(result) and result[-1] == ' ':
-						result = result[0:-1] + text
-					else:
-						result += text
-				break
+                if action == Action.PAD:
+                    result += text + ' '
+                elif action == Action.NOPAD:
+                    if len(result) and result[-1] == ' ':
+                        result = result[0:-1] + text
+                    else:
+                        result += text
+                break
 
-		if not matched:
-			print('ERROR: found unexpected char: ', lua_source[0])
-			exit(1)
-			lua_source = lua_source[1::]
+        if not matched:
+            print('ERROR: found unexpected char: ', lua_source[0])
+            exit(1)
+            lua_source = lua_source[1::]
 
-	if len(result) and result[-1] == ' ':
-		result = result[0:-1]
+    if len(result) and result[-1] == ' ':
+        result = result[0:-1]
 
-	return result
+    return result
 
 
 def generate_full_source(filename: str, remove_debug: bool) -> str:
-	print('Building ' + filename + '...')
+    print('Building ' + filename + '...')
 
-	with open(filename, 'r') as fp:
+    with open(filename, 'r') as fp:
 
-		text = fp.read()
+        text = fp.read()
 
-		while m := require.search(text):
-			fname = m[1].replace('.', '/') + '.lua'
-			with open(fname, 'r') as fp2:
-				text = text[0:m.start(0)] + fp2.read() + text[m.end(0)::]
+        while m := require.search(text):
+            fname = m[1].replace('.', '/') + '.lua'
+            with open(fname, 'r') as fp2:
+                text = text[0:m.start(0)] + fp2.read() + text[m.end(0)::]
 
-		if remove_debug:
-			#Remove all blocks that are marked with debug sections
-			text = debug.sub('', text)
+        if remove_debug:
+            # Remove all blocks that are marked with debug sections
+            text = debug.sub('', text)
 
-		#Replace certain blocks with the contents of a file
-		for i in debug2.findall(text):
-			with open(i[1], 'r') as subfile:
-				subtext = subfile.read().strip().replace('\r', '').replace('\n', '\\n').replace('"', '\\"')
-				# if remove_debug:
-				# 	subtext = subtext.replace('--', '@@@')
-				text = text.replace(i[0], '"' + subtext + '"')
+        # Replace certain blocks with the contents of a file
+        for i in debug2.findall(text):
+            with open(i[1], 'r') as subfile:
+                subtext = subfile.read().strip().replace(
+                    '\r', '').replace('\n', '\\n').replace('"', '\\"')
+                # if remove_debug:
+                # 	subtext = subtext.replace('--', '@@@')
+                text = text.replace(i[0], '"' + subtext + '"')
 
-		if remove_debug:
-			return minify(text)
+        if remove_debug:
+            return minify(text)
 
-		return text
+        return text
 
-#Build the Plasma version of the Paisley engine
+
+# Allow only generating specific files.
+files = ['compiler.lua', 'runtime.lua']
+if len(argv) > 1:
+    files = []
+
+if 'compiler' in argv:
+    files += ['compiler.lua']
+if 'runtime' in argv:
+    files += ['runtime.lua']
+
+# Build the Plasma version of the Paisley engine
 for i in ['compiler.lua', 'runtime.lua']:
-	text = generate_full_source(f'src/{i}', True)
-	module = i.split('.')[0]
-	prefix = f'--[[Paisley {module} v{VERSION}, written by SenorCluckens]]\n--[[This build has been minified to reduce file size]]\n'
+    if i not in files:
+        continue
 
-	with open('build/'+i, 'w') as out:
-		out.write(prefix + text)
+    text = generate_full_source(f'src/{i}', True)
+    module = i.split('.')[0]
+    prefix = f'--[[Paisley {module} v{VERSION}, written by SenorCluckens]]\n--[[This build has been minified to reduce file size]]\n'
 
-#Build the desktop version of the Paisley engine
-text = generate_full_source('paisley', False)
-with open('build/paisley_standalone.lua', 'w') as out:
-	out.write(text)
+    with open('build/'+i, 'w') as out:
+        out.write(prefix + text)
+
+if 'paisley' in argv or 'standalone' in argv:
+    # Build the desktop version of the Paisley engine
+    text = generate_full_source('paisley', False)
+    with open('build/paisley_standalone.lua', 'w') as out:
+        out.write(text)
