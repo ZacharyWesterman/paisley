6c9a6ef06e82b6583b6e0666676685909e6dec54
Zachary Westerman <westerman.zachary@gmail.com>
2025-02-04 19:03:46 -0600

    satisfy linter warning

--- src/compiler/semantics.lua
+++ src/compiler/semantics.lua
@@ -551,11 +551,9 @@ function SemanticAnalyzer(tokens, root_file)
 		if token.text:sub(1, 1) ~= '\\' then return end
 
 		--If function name begins with backslash, it's actually a gosub.
-		-- token.text = 'gosub'
-		-- local args = token.children
-		-- if
+		---@type Token[]
 		local kids = token.children
-		if kids[1].id == TOK.array_concat then kids = kids[1].children end
+		if kids[1].id == TOK.array_concat then kids = kids[1].children or {} end
 		table.insert(kids, 1, {
 			id = TOK.text,
 			text = token.text:sub(2),
