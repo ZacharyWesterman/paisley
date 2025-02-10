12ba07e118174f48ab2c7bfd08e9d919ceb5c919
Zachary Westerman <westerman.zachary@gmail.com>
2025-02-09 20:28:47 -0600

    add array.splice function

--- src/runtime/runtime_functions.lua
+++ src/runtime/runtime_functions.lua
@@ -1091,6 +1091,21 @@ local functions = {
 		local v = POP()
 		PUSH(std.str(v[1]):match(std.str(v[2])))
 	end,
+
+	--SPLICE ARRAY
+	function()
+		local v = POP()
+		local array1, index1, index2, array2 = v[1], std.num(v[2]), std.num(v[3]), v[4]
+		local result = std.array()
+
+		if type(array1) ~= 'table' then array1 = { array1 } end
+		if type(array2) ~= 'table' then array2 = { array2 } end
+
+		for i = 1, index1 - 1 do table.insert(result, array1[i]) end
+		for i = 1, #array2 do table.insert(result, array2[i]) end
+		for i = index2 + 1, #array1 do table.insert(result, array1[i]) end
+		PUSH(result)
+	end,
 }
 
 --[[ INSTRUCTION LAYOUT
