3a5eff8be744562a5aec3f7e7902829a76cd3abc
Zachary Westerman <westerman.zachary@gmail.com>
2025-02-13 19:22:42 -0600

    Add options for building standalone minified lua scripts

--- src/meta/lua.lua
+++ src/meta/lua.lua
@@ -10,12 +10,14 @@ LUA = {
 		local tokens = {}
 
 		local patterns = {
-			{ '^%-%-.-\n',       'comment' }, -- Comments
 			{ '^%-%-%[%[.-%]%]', 'comment' }, -- Multiline Comments
+			{ '^%-%-.-\n',       'comment' }, -- Comments
 			{ '^%[%[.-%]%]',     'string' }, -- Multiline strings
 			{ '^%".-%"',         'string' }, -- Strings
 			{ '^\'.-\'',         'string' }, -- Strings
 			{ '^[%w_]+',         'word' }, -- Words
+			{ '^%(',             'lparen' }, -- Left Parentheses
+			{ '^%)',             'rparen' }, -- Right Parentheses
 			{ '^[%p]',           'symbol' }, -- Punctuation
 			{ '^%s+',            'space' }, -- Whitespace
 		}
@@ -46,8 +48,17 @@ LUA = {
 	--- Minify a string of Lua code.
 	--- @param text string
 	--- @return string
-	minify = function(text)
+	minify = function(text, standalone, remove_delete_blocks)
 		local tokens = LUA.tokenize(text)
+
+		if standalone then
+			tokens = LUA.tokens.replace_requires(tokens)
+		end
+
+		if remove_delete_blocks then
+			tokens = LUA.tokens.remove_delete_blocks(tokens)
+		end
+
 		tokens = LUA.tokens.strip(tokens)
 		return LUA.tokens.join(tokens)
 	end,
@@ -93,10 +104,154 @@ LUA = {
 			end
 			return text
 		end,
+
+		--- Recursively parse require statements and split into a function call and the rest of the program.
+		--- @param tokens LUA.Token[]
+		--- @return LUA.Token[], LUA.Token[]
+		extract_requires = function(tokens)
+			local function match_types(tokens, i, group)
+				for j = 1, #group do
+					-- Ignore whitespace and comments
+					while tokens[i].type == 'space' or tokens[i].type == 'comment' do
+						i = i + 1
+					end
+
+					if tokens[i + j - 1].type ~= group[j] then
+						return false
+					end
+				end
+				return true
+			end
+
+			local function get_next_value(tokens, i, token_type)
+				while tokens[i].type ~= token_type do
+					i = i + 1
+				end
+				return tokens[i].text
+			end
+
+			local function get_next_index(tokens, i, token_type)
+				while tokens[i].type ~= token_type do
+					i = i + 1
+				end
+				return i
+			end
+
+			local require_groups = {}
+			local rqid = 0
+			local new_tokens = {}
+			local i = 1
+			while i <= #tokens do
+				if tokens[i].text == 'require' and match_types(tokens, i + 1, { 'lparen', 'string', 'rparen' }) then
+					local file = get_next_value(tokens, i, 'string'):sub(2, -2):gsub('%.', '/') .. '.lua'
+
+					local fp = io.open(file)
+					if not fp then
+						error('ERROR: File not found: ' .. file)
+					end
+					if not require_groups[file] then
+						rqid = rqid + 1
+						require_groups[file] = {
+							tokens = LUA.tokenize(fp:read('*a')),
+							id = 'RQ' .. rqid,
+						}
+					end
+
+					table.insert(new_tokens, { text = require_groups[file].id, type = 'word' })
+					table.insert(new_tokens, { text = '(', type = 'lparen' })
+					table.insert(new_tokens, { text = ')', type = 'rparen' })
+
+					i = get_next_index(tokens, i, 'rparen')
+					fp:close()
+				elseif tokens[i].text == 'require' and match_types(tokens, i + 1, { 'string' }) then
+					local file = get_next_value(tokens, i, 'string'):sub(2, -2):gsub('%.', '/') .. '.lua'
+					local fp = io.open(file)
+					if not fp then
+						error('ERROR: File not found: ' .. file)
+					end
+					if not require_groups[file] then
+						rqid = rqid + 1
+						require_groups[file] = {
+							tokens = LUA.tokenize(fp:read('*a')),
+							id = 'RQ' .. rqid,
+						}
+					end
+
+					table.insert(new_tokens, { text = require_groups[file].id, type = 'word' })
+					table.insert(new_tokens, { text = '(', type = 'lparen' })
+					table.insert(new_tokens, { text = ')', type = 'rparen' })
+
+					i = get_next_index(tokens, i, 'string')
+					fp:close()
+				else
+					table.insert(new_tokens, tokens[i])
+				end
+				i = i + 1
+			end
+
+			local requires = {}
+			for _, group in pairs(require_groups) do
+				local req_requires, req_tokens = LUA.tokens.extract_requires(group.tokens)
+				for _, token in ipairs(req_requires) do
+					table.insert(requires, token)
+				end
+
+
+				table.insert(requires, { text = 'function', type = 'word' })
+				table.insert(requires, { text = group.id, type = 'word' })
+				table.insert(requires, { text = '(', type = 'lparen' })
+				table.insert(requires, { text = ')', type = 'rparen' })
+
+				for _, token in ipairs(req_tokens) do
+					table.insert(requires, token)
+				end
+
+				table.insert(requires, { text = 'end', type = 'word' })
+			end
+
+			return requires, new_tokens
+		end,
+
+		--- Replace all require calls with the appropriate file contents.
+		--- @param tokens LUA.Token[]
+		--- @return LUA.Token[]
+		replace_requires = function(tokens)
+			local requires, program = LUA.tokens.extract_requires(tokens)
+			for _, token in ipairs(program) do
+				table.insert(requires, token)
+			end
+			return requires
+		end,
+
+		--- Remove any blocks of code that are wrapped in `--[[minify-delete]]` ... `--[[/minify-delete]]` comments.
+		--- @param tokens LUA.Token[]
+		--- @return LUA.Token[]
+		remove_delete_blocks = function(tokens)
+			local new_tokens = {}
+			local i = 1
+			while i <= #tokens do
+				if tokens[i].type == 'comment' and tokens[i].text == '--[[minify-delete]]' then
+					while tokens[i].type ~= 'comment' or tokens[i].text ~= '--[[/minify-delete]]' do
+						i = i + 1
+					end
+				else
+					table.insert(new_tokens, tokens[i])
+				end
+				i = i + 1
+			end
+			return new_tokens
+		end,
 	},
 }
 
--- require 'src.util.filesystem'
--- local fp = FS.open('src/meta/lua.lua', false)
--- LUA.tokens.print(LUA.tokens.strip(LUA.tokenize(fp:read('*a'))))
--- print(LUA.minify(fp:read('*a')))
+local minified = LUA.minify([[
+print('BEGIN')
+require('test1')
+require('test2')
+print('END')
+]], true, true)
+-- tokens = LUA.tokens.replace_requires(tokens)
+-- tokens = LUA.tokens.strip(tokens)
+-- -- LUA.tokens.print(tokens)
+-- print(LUA.tokens.join(tokens))
+print(minified)
