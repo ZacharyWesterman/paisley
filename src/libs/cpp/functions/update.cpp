994c902157572b5ae01133b1beb2003472cfb8ca
Zachary Westerman <westerman.zachary@gmail.com>
2025-03-09 23:13:08 -0500

    CPP build: Fix subvalue updating not working on objects : closes #177

--- src/libs/cpp/functions/update.cpp
+++ src/libs/cpp/functions/update.cpp
@@ -2,35 +2,61 @@
 
 void update(Context &context) noexcept
 {
-	// Replace an element in an array
+	// Replace an element in an array or object
 	auto params = std::get<std::vector<Value>>(context.stack.pop());
-	auto array = params[0].to_array();
-	int index = params[1].to_number();
 	auto value = params[2];
 
-	if (index == 0)
+	if (std::holds_alternative<std::map<std::string, Value>>(params[0]))
 	{
-		context.warn("Array indexes start at 1, not 0. update() will have no effect.");
+		auto object = std::get<std::map<std::string, Value>>(params[0]);
+		auto key = params[1].to_string();
+
+		// If the value is null, remove the key from the object
+		if (std::holds_alternative<Null>(value))
+		{
+			object.erase(key);
+		}
+		else
+		{
+			object[key] = value;
+		}
+
+		context.stack.push(object);
+		return;
+	}
+	else if (std::holds_alternative<std::vector<Value>>(params[0]))
+	{
+		auto array = std::get<std::vector<Value>>(params[0]);
+		int index = params[1].to_number();
+
+		if (index == 0)
+		{
+			context.warn("Array indexes start at 1, not 0. update() will have no effect.");
+			context.stack.push(array);
+			return;
+		}
+
+		if (index < 0)
+		{
+			index += array.size();
+		}
+		else
+		{
+			index -= 1;
+		}
+
+		if (index < 0 || index >= (int)array.size())
+		{
+			context.warn("Array index out of bounds. update() will have no effect.");
+			context.stack.push(array);
+			return;
+		}
+
+		array[static_cast<size_t>(index)] = value;
 		context.stack.push(array);
 		return;
 	}
 
-	if (index < 0)
-	{
-		index += array.size();
-	}
-	else
-	{
-		index -= 1;
-	}
-
-	if (index < 0 || index >= (int)array.size())
-	{
-		context.warn("Array index out of bounds. update() will have no effect.");
-		context.stack.push(array);
-		return;
-	}
-
-	array[static_cast<size_t>(index)] = value;
-	context.stack.push(array);
+	context.warn("First argument to update() must be an array or object. update() will have no effect.");
+	context.stack.push(params[0]);
 }
