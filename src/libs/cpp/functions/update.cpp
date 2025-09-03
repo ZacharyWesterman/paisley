d180b80eba3dca3b024b88d77b3c802327790f64
Zachary Westerman <westerman.zachary@gmail.com>
2025-09-02 21:51:55 -0500

    fix object/array subvalues not being updated correctly

--- src/libs/cpp/functions/update.cpp
+++ src/libs/cpp/functions/update.cpp
@@ -1,62 +1,114 @@
 #include "update.hpp"
+#include <iostream>
 
 void update(Context &context) noexcept
 {
 	// Replace an element in an array or object
 	auto params = std::get<std::vector<Value>>(context.stack.pop());
+	auto object = params[0];
+	auto indices = params[1];
 	auto value = params[2];
+	// bool is_string = false;
 
-	if (std::holds_alternative<std::map<std::string, Value>>(params[0]))
+	// Only valid for arrays, objects, or strings
+	if (std::holds_alternative<std::string>(object))
 	{
-		auto object = std::get<std::map<std::string, Value>>(params[0]);
-		auto key = params[1].to_string();
-
-		// If the value is null, remove the key from the object
-		if (std::holds_alternative<Null>(value))
+		// is_string = true;
+		std::vector<Value> chars;
+		for (auto ch : std::get<std::string>(object))
 		{
-			object.erase(key);
+			chars.push_back(std::string(1, ch));
 		}
-		else
-		{
-			object[key] = value;
-		}
-
+		object = chars;
+	}
+	else if (!std::holds_alternative<std::vector<Value>>(object) && !std::holds_alternative<std::map<std::string, Value>>(object))
+	{
 		context.stack.push(object);
 		return;
 	}
-	else if (std::holds_alternative<std::vector<Value>>(params[0]))
+
+	if (!std::holds_alternative<std::vector<Value>>(indices) && !std::holds_alternative<std::map<std::string, Value>>(indices))
 	{
-		auto array = std::get<std::vector<Value>>(params[0]);
-		int index = params[1].to_number();
-
-		if (index == 0)
-		{
-			context.warn("Array indexes start at 1, not 0. update() will have no effect.");
-			context.stack.push(array);
-			return;
-		}
-
-		if (index < 0)
-		{
-			index += array.size();
-		}
-		else
-		{
-			index -= 1;
-		}
-
-		if (index < 0 || index >= (int)array.size())
-		{
-			context.warn("Array index out of bounds. update() will have no effect.");
-			context.stack.push(array);
-			return;
-		}
-
-		array[static_cast<size_t>(index)] = value;
-		context.stack.push(array);
+		indices = std::vector<Value>{indices};
+	}
+	auto indexes = std::get<std::vector<Value>>(indices);
+	if (indexes.size() == 0)
+	{
+		context.stack.push(object);
 		return;
 	}
 
-	context.warn("First argument to update() must be an array or object. update() will have no effect.");
-	context.stack.push(params[0]);
+	// Narrow down to sub-object
+	Value *sub_object = &object;
+	for (size_t i = 0; i < indexes.size() - 1; i++)
+	{
+		auto ix = indexes[i];
+		if (std::holds_alternative<std::map<std::string, Value>>(*sub_object))
+		{
+			auto s_ix = ix.to_string();
+			auto obj = std::get_if<std::map<std::string, Value>>(sub_object);
+			if (obj->find(s_ix) == obj->end())
+			{
+				// We can only set the bottom-level object
+				context.stack.push(object);
+				return;
+			}
+			sub_object = &(*obj)[s_ix];
+		}
+		else if (!std::holds_alternative<std::vector<Value>>(*sub_object))
+		{
+			context.stack.push(object);
+			return;
+		}
+		else
+		{
+			auto n_ix = ix.to_number() - 1;
+			auto vec = std::get_if<std::vector<Value>>(sub_object);
+			if (n_ix < 0)
+			{
+				n_ix += vec->size();
+			}
+			if (n_ix < 0 || n_ix >= vec->size())
+			{
+				// We can only set the bottom-level object
+				context.stack.push(object);
+				return;
+			}
+			sub_object = &(*vec)[n_ix];
+		}
+	}
+
+	auto ix = indexes.back();
+	if (std::holds_alternative<std::map<std::string, Value>>(*sub_object))
+	{
+		auto obj = std::get_if<std::map<std::string, Value>>(sub_object);
+		auto s_ix = ix.to_string();
+
+		obj->insert_or_assign(s_ix, value);
+	}
+	else if (std::holds_alternative<std::vector<Value>>(*sub_object))
+	{
+		auto vec = std::get_if<std::vector<Value>>(sub_object);
+		auto n_ix = ix.to_number() - 1;
+		if (n_ix < 0)
+		{
+			n_ix += vec->size();
+		}
+		if (n_ix < 0)
+		{
+			// Insert at the beginning
+			vec->insert(vec->begin(), value);
+		}
+		else if (n_ix >= vec->size())
+		{
+			// Append to the end
+			vec->push_back(value);
+		}
+		else
+		{
+			(*vec)[n_ix] = value;
+		}
+	}
+
+	context.stack.push(object);
 }
