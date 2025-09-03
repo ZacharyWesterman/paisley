46502f5a1cbb9dad1c5e0b2c2ebe5d0b6e2703a9
Zachary Westerman <westerman.zachary@gmail.com>
2025-09-02 21:55:15 -0500

    fix stdlib/arg is not parsing arguments correctly in C++ build : closes #187 (#207)
    * fix c++ append() not working like the lua version
    
    * fix object/array subvalues not being updated correctly
    
    * adjust stdlib docs comment
    
    * fix boolean check in stdlib

--- src/libs/cpp/functions/append.cpp
+++ src/libs/cpp/functions/append.cpp
@@ -4,12 +4,6 @@ void append(Context &context) noexcept
 {
 	auto params = std::get<std::vector<Value>>(context.stack.pop());
 	auto lhs = params[0].to_array();
-	auto rhs = params[1].to_array();
-
-	for (auto &value : rhs)
-	{
-		lhs.push_back(value);
-	}
-
+	lhs.push_back(params[1]);
 	context.stack.push(lhs);
 }
