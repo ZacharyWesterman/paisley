cff6dc03b7bf61f799bb4346a8081e40c4edde22
Zachary Westerman <westerman.zachary@gmail.com>
2025-10-05 14:52:38 -0500

    short-cut in reduce() function for 'and' and 'or' operators : closes #213

--- src/libs/cpp/actions/pop_until_null.cpp
+++ src/libs/cpp/actions/pop_until_null.cpp
@@ -1,7 +1,8 @@
 #include "pop_until_null.hpp"
 
-void pop_until_null(VirtualMachine &vm) noexcept
+void _pop_until_null(VirtualMachine &vm) noexcept
 {
+	// Keep popping until we hit a null or the stack is empty
 	while (vm.stack.size())
 	{
 		const auto &top = vm.stack.back();
@@ -10,5 +11,36 @@ void pop_until_null(VirtualMachine &vm) noexcept
 		{
 			break;
 		}
+	}
+}
+
+void pop_until_null(VirtualMachine &vm) noexcept
+{
+	const auto p1 = vm.instructions[vm.instruction_index].operand[0];
+
+	// If p1 is positive, leave that many values on the stack
+	if (p1)
+	{
+		std::vector<Value> keep;
+		for (int i = 0; i < p1 && vm.stack.size(); i++)
+		{
+			if (vm.stack.back().is_null())
+			{
+				break;
+			}
+			keep.push_back(vm.stack.pop());
+		}
+
+		_pop_until_null(vm);
+
+		// Put the kept values back on the stack in reverse order
+		for (auto it = keep.rbegin(); it != keep.rend(); ++it)
+		{
+			vm.stack.push(*it);
+		}
+	}
+	else
+	{
+		_pop_until_null(vm);
 	}
 }
--- src/runtime/runtime_functions.lua
+++ src/runtime/runtime_functions.lua
@@ -1575,7 +1575,21 @@ COMMANDS = {
 
 	--POP STACK UNTIL AND INCLUDING NULL
 	function(line, p1, p2)
+		local keep = {}
+		if p1 then
+			--Keep the N top elements
+			for i = 1, p1 do
+				if STACK[#STACK] == NULL then break end
+				table.insert(keep, POP())
+			end
+		end
 		while POP() ~= nil do end
+		if p1 then
+			--Re-push the N top elements
+			for i = #keep, 1, -1 do
+				PUSH(keep[i])
+			end
+		end
 	end,
 
 	--GET VALUE FROM CACHE IF IT EXISTS, ELSE JUMP
