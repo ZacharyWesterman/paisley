ddbb018c1c1f6ea2f0d79ae17f162843fe03ccf3
Zachary Westerman <westerman.zachary@gmail.com>
2025-10-30 19:57:54 -0500

    Optimize away ternary expressions when possible

--- src/compiler/semantics/pass3.lua
+++ src/compiler/semantics/pass3.lua
@@ -13,6 +13,23 @@ return {
 				end
 			end,
 		},
+
+		[TOK.ternary] = {
+			--Optimize ternaries away if possible
+			function(token, file)
+				local lhs, rhs = token.children[2].value, token.children[3].value
+
+				if lhs == true and rhs == false then
+					token.id = TOK.func_call
+					token.text = 'bool'
+					token.children = { token.children[1] }
+				elseif lhs == false and rhs == true then
+					token.id = TOK.boolean
+					token.text = 'not'
+					token.children = { token.children[1] }
+				end
+			end,
+		},
 	},
 
 	exit = {},
