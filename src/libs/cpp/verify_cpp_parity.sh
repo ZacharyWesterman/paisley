668fb85f088893ef60b2555e69c09c829828d2ce
Zachary Westerman <westerman.zachary@gmail.com>
2025-11-21 12:55:51 -0600

    fix verify to work when installed

--- src/libs/cpp/verify_cpp_parity.sh
+++ src/libs/cpp/verify_cpp_parity.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
+cmd=./../../../paisley
+if [ ! -e "$cmd" ]; then
+	if ! which paisley &>/dev/null; then
+		# Paisley isn't installed on this system;
+		# No way to verify
+		exit 0
+	fi
+
+	cmd=paisley
+fi
+
 # Make sure that all functions have a cpp implementation
-./../../../paisley --introspect --functions | while read -r i; do
+$cmd --introspect --functions | while read -r i; do
 	if [ ! -e functions/"$i".cpp ]; then
 		>&2 echo -e "\e[1;31mERROR:\e[0m Missing C++ implementation of \`$i\` function."
 		exit 1
