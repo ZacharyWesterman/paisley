37a76fe18e43f3177414fc4e7a42b22bb963918d
Zachary Westerman <westerman.zachary@gmail.com>
2026-03-22 19:52:42 -0500

    Add C++ logic for some exception handling

---
+++ src/libs/cpp/actions/get_exception_type.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../virtual_machine.hpp"
+
+void get_exception_type(VirtualMachine &) noexcept;
---
+++ src/libs/cpp/actions/push_exception.cpp
@@ -0,0 +1,16 @@
+#include "push_exception.hpp"
+
+void push_exception(VirtualMachine &vm) noexcept
+{
+	auto &instruction = vm.instructions[vm.instruction_index];
+	auto arg = std::get<std::vector<Value>>(vm.get_const(instruction.operand[0]));
+
+	std::map<std::string, Value> err;
+	err["message"] = arg[0];
+	err["stack"] = std::vector<Value>{instruction.line_no};
+	err["type"] = arg[1];
+	err["file"] = "unknown";
+	err["line"] = instruction.line_no;
+
+	vm.stack.push(err);
+}
