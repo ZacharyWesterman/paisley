02280461db8522f0f43b29d46368a7b0854b9ca2
Zachary Westerman <westerman.zachary@gmail.com>
2026-05-02 17:38:25 -0500

    Split up app config and cmdline options into separate lua files

---
+++ src/app_config.lua
@@ -0,0 +1,6 @@
+return {
+	description = "This is the Paisley compiler and runtime engine.\nhttps://github.com/ZacharyWesterman/paisley",
+	name = "Paisley",
+	exe = "paisley",
+	version = "1.21.9",
+}
