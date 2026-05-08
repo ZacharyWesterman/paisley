e3b08ad5187031b873621106aff29c48c5fe4a2a
Zachary Westerman <westerman.zachary@gmail.com>
2026-05-07 22:28:58 -0500

    Bugfixes/improvements for installation script

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 FAILED=0
 
-if ! which lua &>/dev/null; then
-	echo >&2 'ERROR: Lua is not installed! Please install it and try again.'
-	echo >&2 'On Ubuntu, this would be `apt install lua5.3 liblua5.3-dev`.'
+if ! command -v lua &>/dev/null; then
+	echo >&2 -e '\e[31mERROR\e[0m: Lua is not installed! Please install it and try again.'
+	echo >&2 'On Ubuntu, this would be `apt install lua5.3 liblua5.3-dev luarocks`.'
 	FAILED=1
 fi
 
 #Make sure Lua version is at least 5.3
-if ! which lua5.3 &>/dev/null && ! which lua53 &>/dev/null && ! which lua5.4 &>/dev/null && ! which lua54 &>/dev/null; then
-	echo >&2 'ERROR: Lua version 5.3 or higher is required to install Paisley.'
+if ! command -v lua5.3 &>/dev/null && ! command -v lua53 &>/dev/null && ! command -v lua5.4 &>/dev/null && ! command -v lua54 &>/dev/null; then
+	echo >&2 -e '\e[31mERROR\e[0m: Lua version 5.3 or higher is required to install Paisley.'
 	FAILED=1
 fi
 
-if ! which luarocks &>/dev/null; then
-	echo >&2 'WARNING: luarocks is not installed, so dependencies cannot be installed either. Some features may be missing from this build.'
+if ! command -v luarocks &>/dev/null; then
+	echo >&2 -e '\e[33mWARNING\e[0m: luarocks is not installed, so optional dependencies cannot be installed either.'
+	echo >&2 '         Paisley will still work, but some quality-of-life features will be missing.'
 fi
 
 [ $FAILED == 1 ] && exit 1
@@ -24,7 +25,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
 #Make sure user is running as root
 if [ "$EUID" -ne 0 ]; then
-	echo >&2 'ERROR: This script must be run as root. Aborting install.'
+	echo >&2 -e '\e[31mERROR\e[0m: This script must be run as root. Aborting install.'
 	exit 1
 fi
 
@@ -32,17 +33,27 @@ fi
 ./paisley --install || exit 1
 
 install_dependency() {
-	local rock_name, version, failed
+	local rock_name
+	local version
+	local failed
 	rock_name="$1"
 
-	#Try to install both versions, 5.3 and 5.4, in case one fails.
-	for version in 5.3 5.4; do
-		# Only install the rock if it's not already installed.
-		if luarocks list --lua-version $version "$rock_name" | grep "$rock_name" &>/dev/null; then continue; fi
-		sudo luarocks --lua-version 5.3 install "$1" &>/dev/null
+	local valid_versions=()
+	if command -v lua5.3 &>/dev/null || command -v lua53 &>/dev/null; then
+		valid_versions+=(5.3)
+	fi
+	if command -v lua5.4 &>/dev/null || command -v lua54 &>/dev/null; then
+		valid_versions+=(5.4)
+	fi
 
-		if ! luarocks list --lua-version $version "$rock_name" | grep "$rock_name" &>/dev/null; then
-			echo >&2 "ERROR: Failed to install rock \"$rock_name\" for Lua $version!"
+	#Try to install both versions, 5.3 and 5.4, in case one fails.
+	for version in "${valid_versions[@]}"; do
+		# Only install the rock if it's not already installed.
+		if luarocks list --lua-version $version "$rock_name" 2>/dev/null | grep "$rock_name" &>/dev/null; then continue; fi
+		sudo luarocks --lua-version $version install "$rock_name" &>/dev/null
+
+		if ! luarocks list --lua-version $version "$rock_name" 2>/dev/null | grep "$rock_name" &>/dev/null; then
+			echo >&2 -e "\r\e[31mERROR\e[0m: Failed to install rock \"$rock_name\" for Lua $version!"
 		fi
 	done
 }
@@ -69,9 +80,9 @@ echo -n 'Installing Lua rocks...'
 #Prompt once for password
 if ! sudo echo -n; then
 	echo ' Failed.'
-	echo >&2 'WARNING: Failed to install dependencies: user permission denied.'
+	echo >&2 -e '\e[33mWARNING\e[0m: Failed to install dependencies: user permission denied.'
 	echo >&2 '         It will still work, but some features may be missing.'
-else
+elif command -v luarocks &>/dev/null; then
 	total="$(wc <requires.txt -l)"
 	item=0
 	while read -r rock name; do
@@ -83,8 +94,8 @@ else
 			wait_on_process $! "$rock" "$item" "$total"
 		fi
 	done <requires.txt
-	echo ' Done.'
+	printf '\rInstalling Lua rocks... Done.\n'
 fi
 
 echo
-echo 'Paisley is now installed and ready to use.'
+echo -e '\e[32mPaisley is now installed and ready to use.\e[0m'
