3f2a340eaebbba673f3f600df74bf98c2ae8a4ed
Zachary Westerman <westerman.zachary@gmail.com>
2026-05-14 11:48:17 -0500

    print LSP info about functions before applying constant folding : closes #328 (#333)

--- src/compiler/semantics/lsp_info_post_fold.lua
+++ src/compiler/semantics/lsp_info_post_fold.lua
@@ -1,8 +1,6 @@
 --This module only gets called when running as a language server.
 --It dumps detailed info about various structures, which can be used by vscode or other editors.
 
-local FUNCSIG = require "src.compiler.semantics.signature"
-
 local config = {
 	labels = {},
 	variables = {},
@@ -117,28 +115,6 @@ local function function_text(token)
 	return text
 end
 
-local function func_call_lsp(token, filename)
-	local name = token.text
-	if not BUILTIN_FUNCS[name] then return end
-
-	local funcsig = '**' ..
-		vscode.color(name, vscode.theme.func) .. '**(' .. FUNCSIG(name, true) .. ') &rArr; '
-	if TYPESIG[name].out == 1 then
-		--Return type is the same as 1st param
-		local types = {}
-		for i, k in ipairs(TYPESIG[name].valid) do
-			table.insert(types, k[1])
-		end
-		funcsig = funcsig .. std.join(types, '|', TYPE_TEXT)
-	else
-		---@diagnostic disable-next-line
-		funcsig = funcsig .. TYPE_TEXT(TYPESIG[name].out, true)
-	end
-
-	local text = funcsig .. '\n\n' .. TYPESIG[name].description
-	INFO.hint(token.span, text, filename)
-end
-
 return {
 	init = function(labels, variables)
 		config.labels = labels
@@ -174,14 +150,10 @@ return {
 			function(token, filename)
 				INFO.func_call(token.span, filename)
 			end,
-
-			func_call_lsp,
 		},
 
 		--Print info about each built-in function
 		[TOK.func_call] = {
-			func_call_lsp,
-
 			function(token, filename)
 				if token.text ~= 'env_get' then return end
 
