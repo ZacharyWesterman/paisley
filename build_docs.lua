096150c590cf2abcac2e99c9517a413293f3c65f
Zachary Westerman <westerman.zachary@gmail.com>
2026-05-15 12:34:39 -0500

    update script to write directly to auto-gen docs file

--- build_docs.lua
+++ build_docs.lua
@@ -30,25 +30,52 @@ for category, funcs in pairs(document) do
 end
 table.sort(new_document, function(a, b) return a.category < b.category end)
 
+local filename = 'docs/walkthrough/lang_functions.md'
+local file = io.open(filename, 'r')
+if not file then
+	error('Failed to read ' .. filename)
+end
+local text = file:read('a*')
+if not text then text = '' end
+file:close()
+
+local suffix = text:sub(text:find('%-%-%-') or 0, #text)
+
 -- Print the documentation
-print('# Paisley Standard Library Functions\n')
+file = io.open(filename, 'w')
+if not file then
+	error('Failed to open ' .. filename)
+end
+file:write([[
+# Paisley Standard Library Functions
+
+Note that functions can be called in one of two ways:
+1. The usual syntax, e.g. `split(var, delim)`
+2. Using dot-notation, e.g. `var.split(delim)`
+
+Both are exactly equivalent, the latter syntax is included simply for convenience.
+
+]])
 
 for _, entry in ipairs(new_document) do
 	local title = entry.category:gsub('(%l)(%w*)', function(x, y) return x:upper() .. y end)
 	if title:find(':') then
-		print('### ' .. title:gsub('^(%w+):(%w+)', '%2'))
+		file:write('### ' .. title:gsub('^(%w+):(%w+)', '%2') .. '\n')
 	else
-		print('## ' .. title)
+		file:write('## ' .. title .. '\n')
 	end
 
 	table.sort(entry.funcs, function(a, b) return a.name < b.name end)
 	for _, func in ipairs(entry.funcs) do
 		local return_type = func.info.out
 		if type(return_type) ~= 'table' then return_type = TYPE_ANY end
-		print('- `' ..
+		file:write('- `' ..
 			func.name ..
-			'(' .. function_signature(func.name) .. ') -> ' .. TYPE_TEXT(return_type) .. '`')
-		print('  - ' .. func.info.description)
+			'(' .. function_signature(func.name) .. ') -> ' .. TYPE_TEXT(return_type) .. '`\n')
+		file:write('  - ' .. func.info.description .. '\n')
 	end
-	print()
+	file:write('\n')
 end
+
+file:write(suffix)
+file:close()
