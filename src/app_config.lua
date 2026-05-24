5d6a9493ba89563e16121cd7a5c0dfa1390be24c
Zachary Westerman <westerman.zachary@gmail.com>
2026-05-24 14:56:09 -0500

    increment version

--- src/app_config.lua
+++ src/app_config.lua
@@ -2,5 +2,5 @@ return {
 	description = "This is the Paisley compiler and runtime engine.\nhttps://github.com/ZacharyWesterman/paisley",
 	name = "Paisley",
 	exe = "paisley",
-	version = "1.21.12",
+	version = "1.22.0",
 }
