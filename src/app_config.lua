47e3bc84754ee3590447665aa2ecc963bc507c3e
Zachary Westerman <westerman.zachary@gmail.com>
2026-09-14 13:14:06 -0500

    bump patch number

--- src/app_config.lua
+++ src/app_config.lua
@@ -2,5 +2,5 @@ return {
 	description = "This is the Paisley compiler and runtime engine.\nhttps://github.com/ZacharyWesterman/paisley",
 	name = "Paisley",
 	exe = "paisley",
-	version = "2.1.0",
+	version = "2.1.1",
 }
